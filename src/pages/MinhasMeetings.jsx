539adb52763015e96eb46b4eddddffcf099f72f4
frantecbh <franciscomenezesbh@gmail.com>
2024-03-12 00:22:53 -0300

    mymeeting

---
+++ src/pages/MinhasMeetings.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const MinhasMeetings = () => {
+  return (
+    <div className="mx-auto max-w-6xl my-12 space-y-6 px-5">MinhasMeetings</div>
+  )
+}
--- src/routes/appRoutes.jsx
+++ src/routes/appRoutes.jsx
@@ -3,13 +3,16 @@ import { Home } from '../pages/Home'
 import { Dashboard } from '../pages/Dashboard'
 import { NoMatch } from '../pages/NoMatch'
 import { NewMeeeting } from '../pages/NewMeeeting'
+import { MinhasMeetings } from '../pages/MinhasMeetings'
 
 export const AppRoutes = () => {
   return (
     <Routes>
       {/* <Route path="/" element={<Layout />}> */}
       <Route path="/" element={<Home />} />
-      <Route path="newmeeting" element={<NewMeeeting />} />
+      <Route path="/newmeeting" element={<NewMeeeting />} />
+      <Route path="/mymeeting" element={<MinhasMeetings />} />
+
       <Route path="dashboard" element={<Dashboard />} />
 
       {/* Using path="*"" means "match anything", so this route
